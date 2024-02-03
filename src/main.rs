3c8aa77e3ea80df8f6816156aaba05a02dec08b5
Sérgio Matheus <sergiomatheuss09@gmail.com>
2024-02-03 20:26:39 -0300

    Show response to end user

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use crate::parsing::Response;
 use anyhow::Result;
 use clap::Parser;
+use parsing::Body;
 use reqwest::blocking::get;
 
 mod parsing;
@@ -19,7 +20,21 @@ fn main() -> Result<()> {
 
     let body = get(url)?.text()?;
     let responses: Response = serde_json::from_str(&body)?;
+    let responses = &responses[0];
 
-    println!("{:?}", responses);
+    parse_response(responses);
+
     Ok(())
+}
+
+fn parse_response(body: &Body) {
+    println!("{:<8} -- {}", "Word", body.word);
+    if body.phonetic.is_some() {
+        println!("{:<8} -- {}", "Phonetic", body.phonetic.as_ref().unwrap());
+    }
+    for meaning in body.meanings.iter() {
+        for definitions in meaning.definitions.iter() {
+            println!("{} /", definitions.definition);
+        }
+    }
 }
--- src/parsing.rs
+++ src/parsing.rs
@@ -5,14 +5,14 @@ pub type Response = Vec<Body>;
 #[derive(Debug, Deserialize, Default)]
 pub struct Body {
     pub word: String,
-    pub phonetic: String,
+    pub phonetic: Option<String>,
     pub meanings: Vec<Meaning>,
 }
 
 #[derive(Debug, Deserialize, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct Meaning {
-    pub part_of_speech: String,
+    pub part_of_speech: Option<String>,
     pub definitions: Vec<Definition>,
 }
 
