5fbca825fb5516aab4cf7606b4c34653259ffd94
Sérgio Matheus <sergiomatheuss09@gmail.com>
2024-02-03 16:01:54 -0300

    Request of the api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+use reqwest::blocking::get;
+
+#[derive(Parser)]
+struct Args {
+    name: String,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let url = format!(
+        "https://api.dictionaryapi.dev/api/v2/entries/en/{}",
+        args.name
+    );
+
+    let body = get(url)?.text()?;
+    println!("{:?}", body);
+    Ok(())
 }
